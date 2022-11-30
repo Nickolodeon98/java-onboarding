559baf556c6576ce90ceb2e9b2ea5b92b2c6800a
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-01 00:11:57 +0900

    docs: Problem 2 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
+    public String interpret(String toAnalyze) {
+        Stack<Character> charStack = new Stack<>();
+        char[] separated = toAnalyze.toCharArray();
+
+        for (char c : separated) {
+            if (!charStack.empty() && charStack.peek() == c) {
+                charStack.pop();
+                continue;
+            }
+            charStack.add(c);
+        }
+        return charStack.stream().map(Object::toString).reduce("", (a,b)->String.join("", a, b));
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Problem2 problem2 = new Problem2();
+        answer = problem2.interpret(cryptogram);
         return answer;
     }
 }
