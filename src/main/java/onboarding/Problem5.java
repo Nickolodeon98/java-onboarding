c0e4a54668e6a5e1d706bb93c61936d33fa9a2b5
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-01 01:32:18 +0900

    feat: Problem 5 기능 목록 1번, 2번 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        answer = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < 9) {
+            answer.add(money / units[i]);
+            money -= answer.get(answer.size()-1) * units[i++];
+        }
+
         return answer;
     }
 }
