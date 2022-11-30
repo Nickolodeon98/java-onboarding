776d5bba4ecd7614be15f64978828c371afa844d
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-01 00:45:57 +0900

    feat: Problem 4 기능 목록 1번과 3번 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+
+    interface Alphabet {
+        char size(char alphabet);
+    }
+
+    /* 아스키 코드 상으로 앞에서 몇번째인지 확인하고 뒤에서 몇번째까지 빼서 리턴한다. */
+    public char convertAlphabet(char alphabet, Alphabet alphabetType) {
+        int distFromFront = alphabet - alphabetType.size('A') + 1; // 앞에서 몇번째인지, 예) 'C' 는 67 - 65 + 1 = 3번째이다.
+
+        return (char) (alphabetType.size('Z') - distFromFront + 1); // 앞에서 떨어져 있는만큼 뒤에서 세서 알파벳을 찾아 변환한다. 예) 90 - 'C' (3) + 1 = 88 ('X') 이다.
+    }
+
     public static String solution(String word) {
         String answer = "";
+
+
+
         return answer;
     }
 }
