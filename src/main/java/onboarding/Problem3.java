36081c6901fc41b40541aa6d24bdb16d64985982
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-01 00:25:17 +0900

    docs: Problem 3 기능 목록 1번, 2번 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,27 @@
 package onboarding;
 
 public class Problem3 {
+
+    public int countEachNums(String flow, String target) {
+        int count = 0;
+        while(flow.contains(target)) {
+            flow = flow.replace(target, "");
+            count++;
+        }
+        return count;
+    }
+
+    public int countClap(int num) {
+        int clapCount = 0;
+        for (int i = 1; i <= num; i++) {
+            String numStr = Integer.toString(i);
+            clapCount += countEachNums(numStr, "3")
+                    + countEachNums(numStr, "6")
+                    + countEachNums(numStr, "9");
+        }
+        return clapCount;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
